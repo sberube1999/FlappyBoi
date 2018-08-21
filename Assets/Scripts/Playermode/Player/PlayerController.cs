05f4f569c63d88743e343f2ce6fd504768b88e96
Sam <sberube1999@gmail.com>
2018-08-21 11:33:08 -0400

    etapes 1 a 4

---
+++ Assets/Scripts/Playermode/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private TranslateMover translateMover;
+
+        void Awake()
+        {
+            translateMover = GetComponent<TranslateMover>();
+        }
+        void Start()
+        {
+            var position = transform.position;
+            Debug.Log("Ma position initiale est : " + position);
+        }
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+                translateMover.Move(Vector3.up);
+            if (Input.GetKey(KeyCode.A))
+                translateMover.Move(Vector3.left);
+            if (Input.GetKey(KeyCode.S))
+                translateMover.Move(Vector3.down);
+            if (Input.GetKey(KeyCode.D))
+                translateMover.Move(Vector3.right);
+        }
+    }
+}
